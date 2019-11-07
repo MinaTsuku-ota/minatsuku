09da28683320d7ccb00ee67a221f4ca2ab296673
ikei6256 <ryu.e115752@gmail.com>
2019-11-07 15:32:17 +0900

    minor changes

--- php/phpinfo.php
+++ php/phpinfo.php
@@ -1,2 +1,12 @@
+<html>
+<head>
+  <meta charset ="utf-8">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>phpinfo()だよ</title>
+</head>
+<body>
 <?php
   phpinfo();
+?>
+</body>
+</html>
