fa2547c8beda939727b090c04002d2c1161a2048
kou_ota_MinaTsuku <ser1ous.bo.36dm+ota@gmail.com>
2019-11-07 16:51:45 +0900

    すっきり

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $(function () {
-  
+  $(".b1").hide();
   $('.f1').hide();
-  
   $('.f1').each(function (i) {
-  
-    $(this).delay(1000 * i).fadeIn(1500);
+    $(this).delay(1650 * i).fadeIn(1650);
   });
+});
+
+$(function() {
+  setTimeout(function() {
+  $(".b1").fadeIn(800);
+  }, 3300);
 });
