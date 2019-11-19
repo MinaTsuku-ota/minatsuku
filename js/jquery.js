f3edbc213063a87b946003a98257769a82276626
suke <lucky0606clover+github@gmail.com>
2019-11-19 13:11:56 +0900

    soucetree→sourcetreeに訂正

--- js/jquery.js
+++ js/jquery.js
@@ -5,9 +5,6 @@ $(function(){
     //ナビゲーションをクリックするとメインコンテンツのバッグから―を変更
         a.click(function(){
             contents.removeClass('contents web graphic movie');
-            // contents.removeClass('web');
-            // contents.removeClass('graphic');
-            // contents.removeClass('movie');
             contents.addClass($(this).attr('id'));
         });
     });
