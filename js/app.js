d062d3d49b9086f0a99f591e8718104da332a058
kou_ota_MinaTsuku <ser1ous.bo.36dm+ota@gmail.com>
2019-11-22 17:01:47 +0900

    一旦、プッシュしますー

--- js/app.js
+++ js/app.js
@@ -10,3 +10,27 @@ function imgChange() {
     yjsnpi.classList.remove("afternoonBG"); yjsnpi.classList.add("nightBG");
   }
 }
+
+$(function() {
+  $("#indexTitle div").hide();
+  $("#indexTitle div").each(function(i) {
+    $(this).delay(1000*i).fadeIn(1000);
+  });
+});
+
+// var exitCount = 0;
+// var elemsCount = 0;
+// var indexButton = $("#indexButton")
+// function repeatShow(indexButton) {
+//   elemsCount = indexButton.length;
+//   elems$.eq(exitCount).show("slow", function() {
+//     exitCount++;
+//     if(exitCount < elemsCount) {
+//       repeatShow(elems$);
+//     }
+//   });
+// }
+
+// $(function() {
+//   repeatShow($("indexButton button-01"));
+// });
