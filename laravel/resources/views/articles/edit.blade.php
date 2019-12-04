87e6da94020d761fbb948896b5b97f3019b8ab11
ikei6256 <ryu.e115752@gmail.com>
2019-12-04 14:57:19 +0900

    微修正

--- laravel/resources/views/articles/edit.blade.php
+++ laravel/resources/views/articles/edit.blade.php
@@ -1,4 +1,5 @@
-@extends('layout')
+{{-- @extends('layout') --}}
+@extends('minatsukulayout')
 
 @section('content')
     <h1>Edit: {{ $article->title }}</h1>
--- laravel/resources/views/articles/show.blade.php
+++ laravel/resources/views/articles/show.blade.php
@@ -1,4 +1,5 @@
-@extends('layout')
+{{-- @extends('layout') --}}
+@extends('minatsukulayout')
 
 @section('content')
     <h1>{{ $article->title }}</h1>
