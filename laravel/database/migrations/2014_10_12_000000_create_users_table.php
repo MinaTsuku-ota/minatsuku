246ca84a3ba30832c91828727ee0f3b086bf2749
ikei6256 <ryu.e115752@gmail.com>
2019-12-05 14:48:03 +0900

    tetete

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->unique();
+            $table->string('name');
             // $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('subject');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -64,6 +64,20 @@
                             </div>
                         </div>
 
+                        <!-- 科目選択欄 -->
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                {{Form::select('subject', ['subject1', 'suject2', 'subject3'], null, ['class' => 'form-control'])}}
+                                @error('subject')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
