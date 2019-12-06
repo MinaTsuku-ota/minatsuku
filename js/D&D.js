0dea7a1a31ed904ee039a3ab2f3cc79fb09cc637
suke <>
2019-12-06 16:05:54 +0900

    D&Dプログラム完成

--- js/D&D.js
+++ js/D&D.js
@@ -5,13 +5,46 @@ $(function(){
         imageInput = $('.imageInput'),
         imageOutput = $('.input_span');
 
+    //修正をかけたいグローバル変数（要素番号用）
     var span_num = 0;
 
-    //D&Dエリアをクリックしたらinputクリック
+    //各imageAreaに関数を反映
     imageArea.each(function(){
-        imageArea.on('click',function(e){
-            e.preventDefault();
+        
+        // ----------D&Dぞーん----------
 
+        imageArea.on('dragover',function(ev){
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            //dataTransferがJQOによって使えないので.originalEventとして使えるようにする
+            var drag_ev = ev;
+            if(ev.originalEvent){
+                drag_ev = ev.originalEvent;
+            }
+
+            drag_ev.dataTransfer.dropEffect = 'copy';
+        });
+
+        imageArea.on('dragenter',function(ev){
+            ev.preventDefault();
+            ev.stopPropagation();
+        });
+
+        imageArea.on('drop',function(ev){
+            ev.preventDefault();
+            ev.stopPropagation();
+
+            //dataTransferがJQOによって使えないので.originalEventとして使えるようにする
+            var drag_ev = ev;
+            if(ev.originalEvent){
+                drag_ev = ev.originalEvent;
+            }
+
+            var dt = drag_ev.dataTransfer,
+                files = dt.files;
+
+            //要素番号をグローバル変数に格納
             var name_num = $(this).parent().find('input').attr('name');
 
             if(name_num == 'file0'){
@@ -22,7 +55,25 @@ $(function(){
                 span_num = 2
             };
 
-            console.log(span_num);
+            checkFiles(files);
+        });
+
+        // ----------Clickぞーん----------
+        imageArea.on('click',function(e){
+            //初期動作の"#"を止める
+            e.preventDefault();
+
+            //要素番号をグローバル変数に格納
+            var name_num = $(this).parent().find('input').attr('name');
+
+            if(name_num == 'file0'){
+                span_num = 0;
+            }else if(name_num == 'file1'){
+                span_num = 1;
+            }else if(name_num == 'file2'){
+                span_num = 2
+            };
+
             $(this).parent().find('input').click();
         });
     });
@@ -39,27 +90,30 @@ $(function(){
         if(!file || file.type.indexOf('image/') < 0){
             alert('画像形式が異なります!');
         };
-
+        
         outputImage(file);
     };
 
     //読み込んだ画像を出力
     function outputImage(blob){
-
+        //空のimageインスタンスと、fileからURLを取得するためにバイナリオブジェクトの生成
         var image = new Image(),
             blobURL = URL.createObjectURL(blob);
 
         image.src = blobURL;
 
         $(image).on('load',function(){
-
+            //必ず解放してあげないといけない
             URL.revokeObjectURL(blobURL);
             
+            //要素番号が取得できなかったのでグローバル変数を用いてdivタグにぶちこんでみた
             imageOutput.eq(span_num).find('.imageText').html(image);
         });
+    };
 
-        console.log(blobURL);
 
-    }
+});
 
-})
+// メモ:
+// preventDefault() は、その要素のイベントをキャンセル
+// stopPropagation()は、親要素への伝播をキャンセル
