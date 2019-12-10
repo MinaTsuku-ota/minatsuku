10c487496a46e84b4bb321e539830a539d32c06e
kou_ota_MinaTsuku <ser1ous.bo.36dm+ota@gmail.com>
2019-12-10 23:08:31 +0900

    post.htmlに、インスタントプレビュー機能を組み込みました(画像以外)。

---
+++ laravel/public/js/instantPreview.js
@@ -0,0 +1,38 @@
+function titleReflect() {
+  const kbtit = document.getElementById("ref_tit").value;
+  document.getElementById("pre_tit").textContent = "";
+  document.getElementById("pre_tit").textContent = kbtit;
+}
+
+function genreReflect() {
+  const inm = document.getElementById("pre_gen");
+  const i = document.getElementById("ref_gen");
+  var j = i.selectedIndex;
+  var k = i.options[j].value;
+  var yjsnpi = i.options[j].text;
+  inm.textContent = "";
+  inm.textContent = yjsnpi;
+  if(k == 0) {
+    inm.textContent = "";
+  }
+}
+
+function descriptionReflect() {
+  const kmr = document.getElementById("ref_des").value;
+  document.getElementById("pre_des").textContent = "";
+  document.getElementById("pre_des").textContent = kmr;
+}
+
+function imageReflect() {
+  console.log("geting OK.");
+}
+
+var ppTitEvn = document.getElementById("ref_tit");
+var ppGenEvn = document.getElementById("ref_gen");
+var ppDesEvn = document.getElementById("ref_des");
+var ppImgEvn = document.getElementById("input_td")
+
+ppTitEvn.addEventListener("keyup", titleReflect, false);
+ppGenEvn.addEventListener("change", genreReflect, false);
+ppDesEvn.addEventListener("keyup", descriptionReflect, false);
+ppImgEvn.addEventListener("click", imageReflect, false);
