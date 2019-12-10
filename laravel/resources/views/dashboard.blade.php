1dc18b076f48bfb53405f17718b4bff9b872e1aa
ikei6256 <ryu.e115752@gmail.com>
2019-12-11 08:37:53 +0900

    tetete

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -7,58 +7,47 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!
-                    <h2>ようこそ、{{ Auth::user()->name }}さん！</h2>
-                    <h3>あなたの学科は：{{ App\Subject::find(Auth::user()->subject_id)->subject }}</h3>
-                </div>
-
-                <hr/>
-
-                {{-- コントローラ(DashboardController@index)から送られてくる$articlesを扱う --}}
-                @foreach($articles as $article)
-                <div>
-                    @if(isset($article->image1))
-                    {{-- asset()はlaravel/publicディレクトリへのパスを返す --}}
-                    {{-- "$image"はimagesテーブルの1レコード分のデータ --}}
-                    {{-- "$image->image"はimagesテーブルのimageカラムの内容(画像ファイル名が格納されている) --}}
-                    <img src="{{ asset('storage/'.$article->image1) }}" style="width: 10%; height: auto;"/>
-                    @endif
-                    @if(isset($article->image2))
-                    <img src="{{ asset('storage/'.$article->image2) }}" style="width: 10%; height: auto;"/>
-                    @endif
-                    @if(isset($article->image3))
-                    <img src="{{ asset('storage/'.$article->image3) }}" style="width: 10%; height: auto;"/>
-                    @endif
-                </div>
-                @endforeach
-
-                <hr/>
-
-                <div>
-                <p>遊び</p>
-                <div>
-                    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/tbvxFW4UJdU" frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <form>
-                    @csrf
-                    <p><input type="text"></p>
-                    <p><button type="submit">送信</button></p>
-                </form>
-            </div>
-        </div>
-    </div>
+@if (session('status'))
+<div class="alert alert-success" role="alert">
+    {{ session('status') }}
 </div>
+@endif
+You are logged in!
+<h2>ようこそ、{{ Auth::user()->name }}さん！</h2>
+<h3>あなたの学科は：{{ App\Subject::find(Auth::user()->subject_id)->subject }}</h3>
+
+<hr />
+
+{{-- コントローラ(DashboardController@index)から送られてくる$articlesを扱う --}}
+@foreach($articles as $article)
+<div>
+    @if(isset($article->image1))
+    {{-- asset()はlaravel/publicディレクトリへのパスを返す --}}
+    {{-- "$image"はimagesテーブルの1レコード分のデータ --}}
+    {{-- "$image->image"はimagesテーブルのimageカラムの内容(画像ファイル名が格納されている) --}}
+    <img src="{{ asset('storage/'.$article->image1) }}" style="width: 10%; height: auto;" />
+    @endif
+    @if(isset($article->image2))
+    <img src="{{ asset('storage/'.$article->image2) }}" style="width: 10%; height: auto;" />
+    @endif
+    @if(isset($article->image3))
+    <img src="{{ asset('storage/'.$article->image3) }}" style="width: 10%; height: auto;" />
+    @endif
+</div>
+@endforeach
+
+<hr />
+
+<p>遊び</p>
+<div>
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/tbvxFW4UJdU" frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<form>
+    @csrf
+    <p><input type="text"></p>
+    <p><button type="submit">送信</button></p>
+</form>
 @endsection
