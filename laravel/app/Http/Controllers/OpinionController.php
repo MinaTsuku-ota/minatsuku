46227f1d4691e36535e9a63c1ce4f961803b08be
ryu nemu <ryu.e115752@gmail.com>
2019-12-14 03:46:31 +0900

    opinionビューを反映/他微修正

--- laravel/app/Http/Controllers/OpinionController.php
+++ laravel/app/Http/Controllers/OpinionController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class OpinionController extends Controller
 {
     public function show(){
-        // ご意見ページへリダイレクト
-        return redirect()->route('opinion');
+        // ご意見ページビューを表示
+        return view('opinion');
     }
 
     // ご意見ご要望の投稿処理
