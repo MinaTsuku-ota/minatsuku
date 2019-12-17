f9d817a19ce58b79fa4cf275471a1a032fd00b24
suke <>
2019-12-17 11:25:54 +0900

    ajax機能付きコメント表示

--- laravel/public/js/comment.js
+++ laravel/public/js/comment.js
@@ -25,46 +25,52 @@ $(function() {
 
     });
 
-    // $('.comment_submit').each(function() {
+    $('.comment_submit').each(function() {
 
-    //     $('[type="submit"]').on('click', function(ev) {
+        $('[type="submit"]').on('click', function(ev) {
+            ev.preventDefault();
 
-    //         var thisParent = $(this).parent();
+            var thisParent = $(this).parent();
 
-    //         console.log(thisParent);
+            console.log(thisParent);
 
-    //         var thisParentParent = thisParent.parents('ul');
-    //         var comment_data = thisParent.find('.comment_text').val();
-    //         var comments = "<li><span class='profile'></span> : " + comment_data + "</li>";
+            var thisParentParent = thisParent.parents('ul');
+            var comment_data = thisParent.find('.comment_text').val();
+            var comments = "<li><span class='profile'></span> : " + comment_data + "</li>";
 
-    //         $(comments).appendTo(thisParentParent);
+            $(comments).appendTo(thisParentParent);
 
-    //         console.log(comment_data);
-
-    //     })
-    // })
-
-    $('.comment_text').each(function() {
-
-        $(this).on('click', function() {
-
-            var thisParentParent = $(this).parents('ul');
-            $(thisParentParent).append("<li><span class='profile'></span> : " + "</li>");
-
-        });
-
-        $(this).on('change', function(ev) {
-
-            var thisParentParent = $(this).parents('ul');
-            var thisLi = thisParentParent.find('li');
-
-            var comment_data = ev.val();
-            thisLi.text(comment_data);
+            $.ajax({
+                url: 'sample.php',
+                type: 'GET',
+                datatype: 'text',
+                data: comment_data
+            }).done(function() {
+                console.log('success');
+            })
 
             console.log(comment_data);
 
-        });
+        })
+    })
 
-    });
+    // $('.comment_text').each(function() {
+
+    //     $(this).on('click', function() {
+
+    //         var thisParentParent = $(this).parents('ul');
+    //         $(thisParentParent).append("<li class='output'><span class='profile'></span> : " + "</li>");
+
+    //     });
+
+    //     var $input = $(this),
+    //         $output = $input.parent().find('.output');
+
+    //     $input.on('input', function(event) {
+    //         var value = $input.val();
+    //         $output.append(value);
+    //         console.log($output);
+    //     });
+    // });
 
 })
