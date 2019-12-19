79d384a618f93e1b5f46209a5e705f6b60dcd1c4
ikei6256 <ryu.e115752@gmail.com>
2019-12-19 12:30:47 +0900

    phpコードでBASIC認証を使ってみた

--- laravel/resources/views/dev/dev.blade.php
+++ laravel/resources/views/dev/dev.blade.php
@@ -1,3 +1,15 @@
+<?php
+switch (true) {
+    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+    case $_SERVER['PHP_AUTH_USER'] !== 'admin':
+    case $_SERVER['PHP_AUTH_PW']   !== 'admin':
+        header('WWW-Authenticate: Basic realm="Enter username and password."');
+        header('Content-Type: text/plain; charset=utf-8');
+        die('このページを見るにはログインが必要です');
+}
+header('Content-Type: text/html; charset=utf-8');
+/* 参考記事: https://qiita.com/mpyw/items/dc2cb3632370389d700e */
+?>
 @extends('minatsukulayout')
 
 @section('addcss')
@@ -14,8 +26,7 @@ body {
 <h2>ご意見一覧</h2>
 @foreach($opinions as $opinion)
 <hr/>
-<p>【ユーザ】{{ App\User::find($opinion->user_id)->name }} 【投稿日時】{{ App\Opinion::find($opinion->id)->created_at }}</p>
-<p>【本文】</p>
+<p>【ニックネーム】{{ App\User::find($opinion->user_id)->name }}　【投稿日時】{{ $opinion->created_at }}</p>
 <p>{{ App\Opinion::find($opinion->id)->body }}</p>
 @endforeach
 
