edea4f91e57ba165980ab7117b2816e35e15e6c8
ikei6256 <ryu.e115752@gmail.com>
2019-12-24 13:24:15 +0900

    dashboard修正中 他pull分は終わり

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -1,121 +1,116 @@
-{{-- @extends('layouts.app') --}}
-{{-- @extends('layout') --}}
-@extends('minatsukulayout')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('addcss')
-<link rel="stylesheet" href="/css/new_common.css">
-<style>
-    body {
-        background-image: none;
-        background-color: #fff2f2;
-    }
+<head>
+    <meta charset="utf-8">
+    <title>みなツク -MINATUKU-</title>
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/management.css">
+    <link rel="shortcut icon" href="/image/favicon.png" type="image/png">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/jquery-ui.min.js"></script>
+    <script src="/js/comment.js"></script>
+</head>
 
-</style>
-@endsection
-
-@section('addjs')
-@include('recaptcha_js')
-@endsection
-
-@section('content')
-<h1>Dashboard</h1>
-
-<hr />
-
-<div>
-    <h2>
-        ようこそ、
-        <img src="{{ asset('storage/avaters/'.Auth::user()->avater) }}" alt="avater" style="height: 50px;">
-        {{ Auth::user()->name }}さん！
-    </h2>
-    <h3>あなたの学科は：{{ App\Subject::find(Auth::user()->subject_id)->subject }}</h3>
-    
-    {!! Form::open(['method' => 'PATCH', 'route' => 'dashboard.update', 'files' => true]) !!}
-    <div>
-        {!! Form::label('avater', 'ユーザサムネイルを更新:') !!}
-        {!! Form::file('avater', ['required']) !!}
-    </div>
-    <div>
-        {!! Form::submit('送信') !!}
-    </div>
-    <input type="hidden" name="recaptcha" id="recaptcha">
-    {!! Form::close() !!}
-</div>
-
-<hr />
-
-{{-- コントローラ(DashboardController@index)から送られてくる$articlesを扱う --}}
-@foreach($articles as $article)
-<div>
-    <table class="toukou">
-        <tr>
-            <td colspan="10">タイトル</br>{!! Form::open(['method' => 'DELETE', 'url' => ['articles',
-                $article->id]]) !!}
-                {!! Form::submit('削除') !!}
-                {!! Form::close() !!}
-                <a href="{{ url('articles', $article->id) }}">{{ $article->title }}</a>
-                {{-- <a href="{{ action('DashboardController@edit', [$article->id]) }}">編集</a></td> --}}
-            <a href="{{ route('articles.edit', [$article->id]) }}">編集</a></td>
-        </tr>
-        <tr>
-            <td colspan="6" height="180px">詳細説明<br />{{ $article->body }}</td>
-            <td colspan="4" height="180px"><img src="{{ asset('storage/uploaded_images/'.$article->image1) }}"
-                    alt="no_image"></td>
-        </tr>
-        <tr>
-            <td colspan="1">いいね</td>
-            <td colspan="1" class="comment_button">コメント</td>
-            <td colspan="3">名前</br>{{ App\user::find($article->user_id)->name }}</td>
-            <td colspan="5">
-                科の名前</br>{{ App\subject::find(App\user::find($article->user_id)->subject_id)->subject  }}
-            </td>
-        </tr>
-
-        <tr class="comment-none">
-            <td colspan="10">
-                <ul>
-                    <li>コメント内容の予定</li>
-                </ul>
-            </td>
-        </tr>
-    </table>
-</div>
-@endforeach
-
-<hr />
-
-<div>
-    <p>遊び</p>
-    <div>
-        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/tbvxFW4UJdU" frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-</div>
-<form method="POST" action="{{ route('dashboard.post') }}">
-    @csrf
-    <div class="field">
-        <div class="control">
-            <input type="text" name="text" class="input" placeholder="message" required style="width:500px;">
+<body>
+    <header class="header clearfix">
+        <div class="header-left clearfix">
+            <a href="{{ route('articles.index') }}"><img src="../image/home_daimei.png" class="home_daimei" alt="みなツク"></a>
         </div>
-    </div>
-    <div class="filed">
-        <div class="control">
-            <button type="submit" class="button is-link">送信</button>
+        <div class="header-right">
+            <a href="{{ route('dashboard.index') }}">
+                <div class="btn mypage ">マイページ</div>
+            </a>
+            {{-- クリックされた時に下のlogout-formをsubmitするようにjavascriptで記述しています --}}
+            <div class="btn sinki ">
+                <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+            </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
         </div>
-    </div>
-    <input type="hidden" name="recaptcha" id="recaptcha">
-</form>
+    </header>
+    <main>
+        <div class="header clearfix dummy">
+            <div class="header-left clearfix">
+                <a href="#"><img src="../image/home_daimei.png" class="home_daimei" alt="みなツク"></a>
+            </div>
 
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
-            $notification = $delete.parentNode;
-            $delete.addEventListener('click', () => {
-                $notification.parentNode.removeChild($notification);
-            });
-        });
-    });
+            <div class="header-right">
+                <div class="btn login "><a href="#">ログイン</a></div>
+                <div class="btn sinki "><a href="#">新規登録</a></div>
+            </div>
+        </div>
+        <div id="space">
+            <div id="center">
+                <section>
+                    <div id="profil">
+                        <div id="progazou" class="clearfix">
+                            <img src="{{ asset('storage/avaters/'.Auth::user()->avater) }}" alt="プロフィール画像" id="parson">
+                        </div>
+                        <div id="puro_text">
+                            <div id="nikku">{{ Auth::user()->name }}</div>
+                            <div id="gakka">{{ App\Subject::find(Auth::user()->subject_id)->subject }}</div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
 
-</script>
+        <div id="matome">
+            <div id="toukou">
+                <div id="soutoukou_dai">総投稿</div>
+                <div id="soutoukou">4件</div>
+            </div>
+            <div id="sougou">
+                <div id="good">
+                    <div id="sougood_dai">総グッド</div>
+                    <div id="sougood">?件</div>
+                </div>
+            </div>
+        </div>
 
-@endsection
+        <div id="toukouitiran">
+            <h1>投稿一覧</h1>
+            <div id="toukouPanel">
+
+            
+                <table class="toukou">
+                    <tbody>
+                        <tr>
+                            <td colspan="10">オリジナルテトリス</td>
+                        </tr>
+                        <tr>
+                            <td colspan="6" height="180px">詳細説明</td>
+                            <td colspan="4" height="180px">画像</td>
+                        </tr>
+                        <tr>
+                            <td colspan="1">いいね</td>
+                            <td colspan="1" class="comment_button">コメント</td>
+                            <td colspan="3">名前</td>
+                            <td colspan="5">科の名前</td>
+                        </tr>
+                        <tr class="comment_none">
+                            <td colspan="10">
+                                <ul>
+                                    <li>
+                                        <form action="#">
+                                            <input type="text">
+                                        </form>
+                                    </li>
+                                    <li>コメント内容の予定</li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
+            </div>
+        </div>
+    </main>
+    @include('footer')
+</body>
+
+</html>
