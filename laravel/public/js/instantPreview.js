43788276742eea092ec6264248d415b44f8985ee
kou_ota_MinaTsuku <ser1ous.bo.36dm+ota@gmail.com>
2020-01-21 17:33:29 +0900

    タブ切り替え --> 一旦終了

--- laravel/public/js/instantPreview.js
+++ laravel/public/js/instantPreview.js
@@ -2,6 +2,12 @@
 	// let img_obj = new Map([["img01",""],["img02",""],["img03",""]]);
 	var img_obj = {img0:"",img1:"",img2:""};
   var preButton = document.getElementById("gR");
+
+  // 画像URIを格納
+  var imageText = document.getElementsByClassName("imageText");
+  for(let i=0; i<imageText.length; i++){
+    
+  }
   
   preButton.addEventListener("click", ReflectToPreview, false);
   function ReflectToPreview(e){
@@ -12,11 +18,11 @@
     document.getElementById("pre_tit").textContent = "";
     document.getElementById("pre_tit").textContent = kbtit;
     //ジャンルの値をプレビューに渡す
-    const inm = document.getElementById("pre_gen");
-    const i = document.getElementById("ref_gen");
-    var j = i.selectedIndex;
-    var k = i.options[j].value;
-    var yjsnpi = i.options[j].text;
+    const inm = document.getElementById("pre_gen"),
+          i = document.getElementById("ref_gen");
+    var j = i.selectedIndex,
+        k = i.options[j].value,
+        yjsnpi = i.options[j].text;
     inm.textContent = "";
     inm.textContent = yjsnpi;
     if(k == 0) {
@@ -26,52 +32,5 @@
     const kmr = document.getElementById("ref_des").value;
     document.getElementById("pre_des").textContent = "";
     document.getElementById("pre_des").textContent = kmr;
-		// 画像をプレビューに渡す
-    var img0 = document.querySelector("#input_td > span:nth-child(1) > div").firstElementChild;
-    var img1 = document.querySelector("#input_td > span:nth-child(2) > div").firstElementChild;
-    var img2 = document.querySelector("#input_td > span:nth-child(3) > div").firstElementChild;
-    var pre0 = document.getElementsByClassName("preImgPosition1");
-    var pre1 = document.getElementsByClassName("preImgPosition2");
-    var pre2 = document.getElementsByClassName("preImgPosition3");
-    var span = document.createElement("span");
-    // 短くしたい
-		var files = e.target.files;
-		var reader = new FileReader;
-    if(img0 && !img1 && !img2){
-      img_obj.set("img01",img0);
-      img_obj.set("img02","");
-      img_obj.set("img03","");
-    }else if(img0 && img1 && !img2){
-      img_obj.set("img01",img0);
-      img_obj.set("img02",img1);
-      img_obj.set("img03","");
-    }else if(img0 && img1 && img2){
-      img_obj.set("img01",img0);
-      img_obj.set("img02",img1);
-      img_obj.set("img03",img2);
-    }else if(!img0 && img1 && !img2){
-      img_obj.set("img01","");
-      img_obj.set("img02",img1);
-      img_obj.set("img03","");
-    }else if(!img0 && img1 && img2){
-      img_obj.set("img01","");
-      img_obj.set("img02",img1);
-      img_obj.set("img03",img2);
-    }else if(!img0 && !img1 && img2){
-      img_obj.set("img01","");
-      img_obj.set("img02","");
-      img_obj.set("img03",img2);
-    }else if(!img0 && img1 && !img2){
-      img_obj.set("img01","");
-      img_obj.set("img02",img1);
-      img_obj.set("img03","");
-    }else if(img0 && !img1 && img2){
-      img_obj.set("img01",img0);
-      img_obj.set("img02","");
-      img_obj.set("img03",img2);
-    }else{
-      img_obj.set([["img01",""],["img02",""],["img03",""]]);
-    }
-    console.log(img_obj.entries());
   } // <-- function end
   // end
--- laravel/public/js/tagSwitch.js
+++ laravel/public/js/tagSwitch.js
@@ -1,9 +1,14 @@
-function ppp(){
+function aaa(){
 	// URL解析(練習用)
 	// 一旦中止、bladeの変更だけでできそうなので・・・(;^o^)<蛇足...
-	var murl = /http(s)?:\/\/localhost\/([\w- ./?%&=]*)/gi;
+	var murl = /http(s)?:\/\/localhost\/([\w- ./?%&=]*)/gi,
+		murl2 = /http(s)?:\/\/localhost:([0-9])*\/([\w- ./?%&=]*)/gi;
 	var htsrc = document.body.innerHTML;
-	var result = htsrc.match(murl); console.log(result);
+	var result = htsrc.match(murl),
+		result0 = htsrc.match(murl2);
+		if(result0[0].match(/:8080/g)){
+			console.log(result0);
+		}else{console.log(result);}
 	// ページネーション機能
 	const purl = /http(s)?:\/\/([\w-]*)\/articles([\d]*)[?]id=([1-9]+)/gi;
 	var result2 = htsrc.match(purl); console.log(result2);
@@ -11,23 +16,30 @@ function ppp(){
 	// for(var i=0, len=result2.length; i<len; i++){
 	// }
 }
-window.onload = ppp;
+window.onload = aaa;
 
 document.addEventListener('DOMContentLoaded', function(){
 	// genreタブに対してイベントリスナー
-	const tabs = document.getElementsByClassName('article-tab');
-	for(let i=0; i<tabs.length; i++) {
-		tabs[i].addEventListener('click', tabSwitch);
+	var toukouPanel = document.getElementsByClassName('toukouPanel'),
+			panel_show = document.getElementsByClassName('panel-show'),
+			article_tab = document.getElementsByClassName('article-tab'),
+			tag_active = document.getElementsByClassName('tag-active'),
+			radio = document.getElementById("radio"),
+			tabs = radio.tabs;
+	for(let i=0; i<article_tab.length; i++) {
+		article_tab[i].addEventListener('click', tabSwitch, false);
 	}
 	
 	function tabSwitch(){
-		// genreタブの切り替え(いらなくね？)
-		document.getElementsByClassName('tag-active')[0].classList.remove('tag-active');
-		this.classList.add('tag-active');
+		// for(var i=0; i<4; i++){
+		// 	tabs[i].checked = false;
+		// }
+		// genreタブの切り替え(既にcheckedのcssが存在していたので、addClassは飾りです)
+		// this.classList.add('tag-active');
 		// toukouPanelの切り替え
-		document.getElementsByClassName('panel-show')[0].classList.remove('panel-show');
-		const arrayTabs = Array.prototype.slice.call(tabs);
-		const index = arrayTabs.indexOf(this);
-		document.getElementsByClassName('article-panel')[index].classList.add('panel-show');
+		panel_show[0].classList.remove('panel-show');
+		var arrayTabs = Array.prototype.slice.call(article_tab),
+				index = arrayTabs.indexOf(this);
+		toukouPanel[index].classList.add('panel-show');
 	}
 });
