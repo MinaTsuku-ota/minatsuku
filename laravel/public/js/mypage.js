a7014f2896ecb77b09f63c83303729abf103ab15
suke <>
2020-01-22 16:04:32 +0900

    management.htmlの拡張機能(活動実績)

--- laravel/public/js/mypage.js
+++ laravel/public/js/mypage.js
@@ -7,50 +7,101 @@ $(function() {
     var $profile = $('#profile_js');
     var profile_position = $profile.position();
 
-    //画像オブジェクトの生成
-    var jellyfish = new Image();
-    jellyfish.src = '/image/test4.png';
+    //表示エリアの定義
+    var $area = $('#area_js');
+    var area_position = $area.offset();
 
     //表示位置の計算
-    var top = space_position.top + Math.random() * 900;
-    var left = space_position.left + Math.random() * 900;
+    var top = area_position.top;
+    var left = area_position.left;
+
+    //境界線の定義
+    var scroll_border = top - $(window).height();
+
+    //画像オブジェクトの生成
+    var jellyfish = new Image();
+    jellyfish.src = '/image/test.png';
 
     //投稿数の取得
-    var web_num = $('#post_js').text();
-    web_num = parseInt(web_num, 10);
-    console.log(web_num);
+    var post_num = $('#post_js').text();
+    post_num = parseInt(post_num, 10);
 
     //いいね数の取得
     var good_num = $('#good_js').text();
     good_num = parseInt(good_num, 10);
     var good_num_size = good_num / 10;
-    console.log(good_num);
+
+    //ジャンプアニメーションで使う切り替え用flag
+    var flag = false;
 
     //投稿数に応じた画像の表示
-    for (var i = 0; i < web_num; i++) {
-        $space.append('<div class="jelly"></div>').find('.jelly').html(jellyfish);
+    for (var i = 0; i < post_num; i++) {
+        $area.append('<div class="jelly"></div>').find('.jelly').html(jellyfish);
     };
 
     //ドラッグ可能にするjQuery.UI
     $('.jelly').draggable({
-        containment: 'parent',
         scroll: false,
     });
 
+    //個別設定
     $('.jelly').each(function() {
-
         //表示位置の設定
-        $(this).offset({ top: space_position.top + Math.random() * 900, left: space_position.left + Math.random() * 900 });
+        var index = $(this).index();
+        $(this).offset({ top: 100 + Math.random() * 20, left: left + index * 60 });
 
         //いいね数に応じてサイズを変える
         $(this).find('img').css({
             'height': 30 * good_num_size + Math.random(),
-            'width': 30 * good_num_size + Math.random()
+            'width': 20 * good_num_size + Math.random()
         });
 
-        $(this).fadeIn(1000 * Math.random() + 1000);
+        //表示速度をバラバラにする
+        $(this).fadeIn(1000 * Math.random() * 5);
+
+        //初期ジャンプアニメーションの発火
+        up($(this), 0.5);
 
     });
+
+    //クリック時破裂
+    $('.jelly').on('click', function() {
+
+        $(this).stop(true).animate({
+            top: '+=0px'
+        }, 0).animate({
+            top: '-=500px',
+            left: '+=' + (Math.floor(Math.random() * 500) - 100) + 'px'
+        }, 1500);
+
+        $(this).stop().effect('explode', '', 1000);
+    })
+
+    //画面トップでスクロール時にアニメーション発火
+    $(window).scroll(function() {
+        if (space_position.top > $(window).scrollTop() && flag === false) {
+            flag = true;
+            $('.jelly').each(function() {
+                up($(this), Math.random() * 5);
+                console.log('ジャンプ');
+            });
+        } else if (space_position.top < $(window).scrollTop() && flag === true) {
+            flag = false;
+        };
+    })
+
+    //アニメーションの繰り返し処理
+    function up(is, num) {
+        for (var i = 0; i < 3; i++) {
+            is.animate({
+                top: '+=0px'
+            }, 1000 + num * 700).animate({
+                top: '-=15px'
+            }, 250).animate({
+                top: '+=15px'
+            }, 500);
+        };
+    };
 
     console.log(space_position);
     console.log(profile_position);
